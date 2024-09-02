1d4266bdd9b669891ac606d2e1579d9ae933acb4
eduardonk9999 <thiago.novak7@gmail.com>
2024-09-02 09:49:21 -0300

    feat= class principal

---
+++ src/main/java/org/example/powerrangers/powerRanger.java
@@ -0,0 +1,31 @@
+package org.example.powerrangers;
+
+public class powerRanger {
+    private String cor;
+    private String nome;
+    private String arma;
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getArma() {
+        return arma;
+    }
+
+    public void setArma(String arma) {
+        this.arma = arma;
+    }
+}
